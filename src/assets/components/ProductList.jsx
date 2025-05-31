7c4de76fa85088a9416c0f5385371aaf7e1119c5
akankshasalokhe <akankshasalokhe2001@gmail.com>
2025-05-31 07:35:12 +0530

    Update ProductList.jsx

--- src/assets/components/ProductList.jsx
+++ src/assets/components/ProductList.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React , { useEffect, useState } from "react";
-import UpdateProduct from "./updateProduct";
+import UpdateProduct from "./UpdateProduct";
 
 
 function ProductList() {
